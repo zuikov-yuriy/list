27926c8d45a8e8621b578fe2795410391b8fa4b4
ubuntu <ubuntu@ubuntu.(none)>
2012-10-29 09:36:15 +0200

    format

--- list.rb
+++ list.rb
@@ -1,11 +1,15 @@
 class Node
+
   attr_reader :data, :go
+
   def initialize(data)
      @data = data
   end
+
   def link(go)
      @go = go
   end
+
 end
 
 
@@ -13,37 +17,33 @@ end
 
 class List
 
-attr_reader :start_element, :end_element
+   attr_reader :start_element, :end_element
 
    def initialize
-     @head = 0
-	 #@size = 5
+      @head = 0
+      #@size = 5
    end 
 
    def add(obj)
 
-     if @head == 0
-          obj.link 0
-		  @start_element = obj
-		  @head = 1
-	   else
-	      @end_element.link obj
-	      obj.link 0
-	  end	 
-	  
-	 @end_element = obj
-	 
+      if @head == 0
+         obj.link 0
+         @start_element = obj
+	 @head = 1
+      else
+	 @end_element.link obj
+	 obj.link 0
+      end	 
+
+     @end_element = obj 
    end
 
    
-   
 
-   
-   
    def view_next(obj)
-     @this = obj
-	 
-	 puts "SELECT ELEMENT"
+      @this = obj
+
+         puts "SELECT ELEMENT"
 	 puts "#{@this.data}"
 
 	 if @this == @end_element
@@ -54,41 +54,26 @@ attr_reader :start_element, :end_element
 	 end 
 	 
    end
-   
-   
-   def start(el)
-   end
-   
-   
-   
+ 
+
    def all
-         @start = @start_element
-		 @end = @end_element
-         @obj = nil
-		 
-		 loop do
-		   if @obj == nil
-		       @data = @start
-			   @obj = @start.go
-		   else  
- 		       @data = @obj
-			   @obj = @obj.go
-		   end
-			
-			puts @data.data
-			
-			     @e = @data
-	
-		    break if @e == @end
 
-		end
+      @obj = nil
 
+         loop do
+           if @obj == nil
+	      @data = @start_element
+	      @obj = @start_element.go
+	    else  
+ 	      @data = @obj
+	      @obj = @obj.go
+	   end
+			
+	   puts @data.data
+           @e = @data
+           break if @e == @end_element
+         end
    end
-
-   
-   
-   
-   
 
 end
 
@@ -103,8 +88,8 @@ node5 = Node.new('ELEMENT - 5')
 
 list = List.new
 list.add(node1)
-list.add(node2)
 list.add(node3)
+list.add(node2)
 list.add(node4)
 list.add(node5)
 
@@ -115,7 +100,7 @@ list.add(node5)
 
 #list.all
 
-list.view_next(node5)
+list.view_next(node2)
 
 
 
